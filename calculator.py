26474e9b61b7fc293b930d76151fda3694bbee7b
Lgeorgiev <lgeorgiev059@gmal.com>
2024-11-13 16:07:41 +0200

    simple

---
+++ calculator.py
@@ -0,0 +1,8 @@
+print("Welcome to the simple calculator")
+
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+result = float(num1) + float(num2)
+
+print("The sum of", num1, "and", num2, "is:", result)
