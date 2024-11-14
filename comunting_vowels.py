fd277cef5dd1fe9e4df17a1a8fec19f8eddc7129
Lgeorgiev <lgeorgiev059@gmal.com>
2024-11-14 15:09:16 +0200

    add .lower() and simplify the code

--- comunting_vowels.py
+++ comunting_vowels.py
@@ -5,13 +5,13 @@ Ask the user to enter a sentence.
 Count the vowels (a, e, i, o, u) in the sentence.
 Print the number of vowels found."""
 
-sentence = input("Enter the sentence to count vowels: ").strip()
+sentence = input("Enter the sentence to count vowels: ").strip().lower()
 
 vowels = "aeiou"
 vowels_count = 0
 
 for j in sentence:
-    for x in vowels:
-        if x == j:
-            vowels_count += 1
+    if j in vowels:
+        vowels_count += 1
+        
 print("Total vowels:", vowels_count)
